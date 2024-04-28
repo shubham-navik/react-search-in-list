fca2fce7a9881d51fb1d20df66d7af47a51d0b2c
shubham-navik <112787977+shubham-navik@users.noreply.github.com>
2024-04-28 12:12:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import names from './Name'; // Assuming './Name' contains your array of names
 
 function App() {
+
+  const [searchTerm, setSearchTerm] = useState('');
+  const [selectGender, setSelectGender] = useState('all');
+  const [searchResult, setSearchResult] = useState([]);
+
+  const handleChange = (e) => {
+    const term = e.target.value;
+    setSearchTerm(term);
+    const results = names.filter(name => {
+      if (selectGender !== 'all' && selectGender !== name.gender) {
+        return false;
+      }
+      return name.name.toLowerCase().includes(term.toLowerCase());
+    });
+    setSearchResult(results);
+  }
+
+  const handleClick = (gender) => {
+    setSelectGender(gender);
+    setSearchTerm('');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello </h1>
+      <button onClick={() => handleClick('all')}>All</button>
+      <button onClick={() => handleClick('male')}>Boys</button>
+      <button onClick={() => handleClick('female')}>Girls</button>
+      <input type='text' value={searchTerm} onChange={handleChange} />
+      
+      <ul>
+        {searchResult.map((name, index) => (
+          <li key={index}>{name.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ src/Name.js
@@ -0,0 +1,44 @@
+const names = [
+    {
+        "name": "John",
+        "gender": "male"
+    },
+    {
+        "name": "Emma",
+        "gender": "female"
+    },
+    {
+        "name": "Michael",
+        "gender": "male"
+    },
+    {
+        "name": "Sophia",
+        "gender": "female"
+    },
+    {
+        "name": "William",
+        "gender": "male"
+    },
+    {
+        "name": "Olivia",
+        "gender": "female"
+    },
+    {
+        "name": "James",
+        "gender": "male"
+    },
+    {
+        "name": "Ava",
+        "gender": "female"
+    },
+    {
+        "name": "Alexander",
+        "gender": "male"
+    },
+    {
+        "name": "Mia",
+        "gender": "female"
+    }
+];
+
+export default names;
